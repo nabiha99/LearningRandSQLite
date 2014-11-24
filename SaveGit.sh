59a467adef6e45cc4fc9b272d72c05a7e584bbbd
nabiha99 <huqnp@uw.edu>
2014-11-24 15:08:07 -0800

    script to save to Git repository

---
+++ SaveGit.sh
@@ -0,0 +1,4 @@
+#Script to save changes to Git repository
+sudo git add SuggestionsforLearningRandSQlite.Rmd 
+sudo git commit -m "revise SuggestionsforLearningRandSQLite.Rmd" 
+sudo git push -u origin master #push changes to online repository
